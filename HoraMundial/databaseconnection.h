475094aacdee71ccd4e73b71bd9e7dccb292cd09
Cristian Murcia <cristian47@gmail.com>
2020-11-05 19:22:32 -0300

    Pasaje de desarrollo repositorio roto

---
+++ HoraMundial/databaseconnection.h
@@ -0,0 +1,22 @@
+#ifndef DATABASECONNECTION_H
+#define DATABASECONNECTION_H
+
+#include <QSqlDatabase>
+
+class DataBaseConnection {
+public:
+    DataBaseConnection(const QString &databaseName,
+                       const QString &port,
+                       const QString &user,
+                       const QString &password);
+
+private:
+    QSqlDatabase mDatabase;
+    QString mDatabaseName;
+    QString mPort;
+    QString mUser;
+    QString mPassword;
+
+};
+
+#endif // DATABASECONNECTION_H
